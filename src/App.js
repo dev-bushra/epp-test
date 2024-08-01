743f17af13e3d4fd99c59b1ef4ead2a3bfee3ffe
bushra-dev <bushra.ebox@gmail.com>
2024-08-01 18:12:49 +0400

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css"
+
+import { Box, Button, Text } from '@chakra-ui/react';
+
+function App() {
+  return (
+    <div className="login-wrapper flex">
+      Hello
+    </div>
+  );
+}
+
+export default App;
